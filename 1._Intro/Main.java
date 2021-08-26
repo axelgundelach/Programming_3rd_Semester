797a285f77ea9f77af06f3ec1cb6d304248306cc
Axel <axelgunde@gmail.com>
2021-08-26 10:29:59 +0200

    Task 1.

--- 1._Intro/Main.java
+++ 1._Intro/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args ) {
 
+        Main.print();
+    }
+
+    public static void print(){
+       System.out.print("Dette er en string");
     }
 }
